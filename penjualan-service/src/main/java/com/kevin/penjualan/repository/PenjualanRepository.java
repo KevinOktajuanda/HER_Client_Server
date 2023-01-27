eb1253705d3b85b5f9156834cffbad345f6faa13
KevinOktajuanda <113493565+KevinOktajuanda@users.noreply.github.com>
2023-01-27 16:51:52 +0700

    Add files via upload

---
+++ penjualan-service/src/main/java/com/kevin/penjualan/repository/PenjualanRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.kevin.penjualan.repository;
+
+import com.kevin.penjualan.entity.Penjualan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ASUS
+ */
+@Repository
+public interface PenjualanRepository extends JpaRepository<Penjualan, Long>{
+    public Penjualan findByPenjualanId(Long penjualanId);
+
+}
