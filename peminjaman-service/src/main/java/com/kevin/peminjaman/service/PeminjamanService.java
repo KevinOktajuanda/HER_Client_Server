883ce6e68917d89ff7b8c11af35d09a4b4c67c9a
KevinOktajuanda <113493565+KevinOktajuanda@users.noreply.github.com>
2023-01-27 16:48:24 +0700

    Add files via upload

---
+++ peminjaman-service/src/main/java/com/kevin/peminjaman/service/PeminjamanService.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kevin.peminjaman.service;
+
+import com.kevin.peminjaman.VO.Anggota;
+import com.kevin.peminjaman.VO.Buku;
+import com.kevin.peminjaman.VO.ResponseTemplateVO;
+import com.kevin.peminjaman.entity.Peminjaman;
+import com.kevin.peminjaman.repository.PeminjamanRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author ASUS
+ */
+@Service
+public class PeminjamanService {
+     @Autowired
+    private PeminjamanRepository peminjamanRepository;
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    
+   
+    public Peminjaman savePeminjaman(Peminjaman peminjaman) {
+       return peminjamanRepository.save(peminjaman); 
+    }
+
+     public ResponseTemplateVO getPeminjaman(Long peminjamanId){
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        Peminjaman peminjaman = peminjamanRepository.findByPeminjamanId(peminjamanId);
+        Anggota anggota = restTemplate.getForObject("http://localhost:9012/anggota/" 
+              + peminjaman.getAnggotaId(), Anggota.class);
+        Buku buku = restTemplate.getForObject("http://localhost:9003/buku/" 
+                + peminjaman.getBukuId(), Buku.class);
+        vo.setPeminjaman(peminjaman);
+        vo.setAnggota(anggota);
+        vo.setBuku(buku);
+        return vo;
+    }
+     
+     public Peminjaman findPeminjamanById(Long peminjamanId){
+        return peminjamanRepository.findByPeminjamanId(peminjamanId);
+    } 
+     
+    public List<Peminjaman> getAllPeminjaman(){
+        return peminjamanRepository.findAll();
+    }
+    
+    public void deletePeminjaman(Long peminjamanId) {
+        peminjamanRepository.deleteById(peminjamanId);
+    }
+    
+    public Peminjaman updatePeminjaman(Peminjaman peminjaman){
+        return peminjamanRepository.save(peminjaman);
+    }
+}
